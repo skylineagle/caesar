cac1bfc7e3ec40621c1c97fee5b2fad65a301c1f
skylineagle <skylineagle@proton.me>
2025-07-07 19:57:04 +0300

    tweak: brought back removed code

--- web/src/hooks/use-overlay-state.tsx
+++ web/src/hooks/use-overlay-state.tsx
@@ -1,24 +1,27 @@
+import { useEffect, useMemo } from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
+
 export function useSearchEffect(
   key: string,
   callback: (value: string) => boolean,
 ) {
-  console.log("useSearchEffect", key);
-  // const location = useLocation();
-  // const [searchParams, setSearchParams] = useSearchParams();
-  // const param = useMemo(() => {
-  //   const param = searchParams.get(key);
-  //   if (!param) {
-  //     return undefined;
-  //   }
-  //   return [key, decodeURIComponent(param)];
-  // }, [searchParams, key]);
-  // useEffect(() => {
-  //   if (!param) {
-  //     return;
-  //   }
-  //   const remove = callback(param[1]);
-  //   if (remove) {
-  //     setSearchParams(undefined, { state: location.state, replace: true });
-  //   }
-  // }, [param, location.state, callback, setSearchParams]);
+  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const param = useMemo(() => {
+    const param = searchParams.get(key);
+    if (!param) {
+      return undefined;
+    }
+    return [key, decodeURIComponent(param)];
+  }, [searchParams, key]);
+
+  useEffect(() => {
+    if (!param) {
+      return;
+    }
+    const remove = callback(param[1]);
+    if (remove) {
+      setSearchParams(undefined, { state: location.state, replace: true });
+    }
+  }, [param, location.state, callback, setSearchParams]);
 }
