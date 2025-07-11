18f52349df379c810b5556037d3df85b0b16ca14
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2025-07-11 08:11:35 -0500

    Set ulimit with Python (#19105)
    * Set ulimit with python instead of in s6 startup
    
    * move to services and add env var
    
    * add comment

--- frigate/util/services.py
+++ frigate/util/services.py
@@ -761,10 +761,10 @@ def set_file_limit() -> None:
     soft_limit = int(os.getenv("SOFT_FILE_LIMIT", "65536") or "65536")
 
     current_soft, current_hard = resource.getrlimit(resource.RLIMIT_NOFILE)
-    logger.debug(f"Current file limits - Soft: {current_soft}, Hard: {current_hard}")
+    logger.info(f"Current file limits - Soft: {current_soft}, Hard: {current_hard}")
 
     new_soft = min(soft_limit, current_hard)
     resource.setrlimit(resource.RLIMIT_NOFILE, (new_soft, current_hard))
-    logger.debug(
+    logger.info(
         f"File limit set. New soft limit: {new_soft}, Hard limit remains: {current_hard}"
     )
